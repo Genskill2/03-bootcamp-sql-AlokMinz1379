b4331603febc5ac7cc6bce94d7229aeaae316ef1
Alok Minz <alokminz321@gmail.com>
2021-06-20 20:32:57 +0530

    attempt

--- create.sql
+++ create.sql
@@ -1,24 +1,24 @@
 create table publisher(
-          id serial PRIMARY KEY,
+          id INTEGER PRIMARY KEY,
           name text,
           country text
           );
           
 create table books(
-        id serial PRIMARY KEY,
+        id INTEGER PRIMARY KEY,
         title   text,
-        publisher serial REFERENCES publisher(id)
+        publisher INTEGER REFERENCES publisher(id)
         );
         
       
 create table subjects(
-        id serial PRIMARY KEY,
+        id INTEGER PRIMARY KEY,
         name text
         );
         
 create table books_subjects(
-        book serial REFERENCES books(id),
-        subject serial REFERENCES subjects(id)
+        book INTEGER REFERENCES books(id),
+        subject INTEGER REFERENCES subjects(id)
         );                
                   
           
